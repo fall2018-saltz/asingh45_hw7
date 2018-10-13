096e615beddfff572791ca56e97abcfccaef5eb6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 20:04:15 -0400

    Saving read_census.R

--- read_census.R
+++ read_census.R
@@ -1,2 +1,11 @@
 
-clean_data <- raw_data
+#Here we read our census data into a dataframe and further clean in 
+#by removing unwanted rows, columns and giving them logical names
+clean_df <- function(){
+  census <- data.frame(census)
+  census <- census[-c(1,53),c(5:8)]
+  colnames(census) <- c("stateName","population","popOver18","percentOver18")
+  return(census)
+}
+#Finall we save it in a dataframe object and pass it on for further processing
+census <- clean_df()
