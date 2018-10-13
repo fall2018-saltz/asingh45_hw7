170e324307a0d4edaa51ed7e3216dfaf31eca8ea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 20:34:27 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -65,6 +65,7 @@ nyy <- df[df$stateName=="New York",'StateCenterY']
 
 #Next we use the earlier approach to make the map 
 #however also  add the limits using xlim and ylim
+#in coord_map and set these to a value +/-10 from 
 ggplot(df,aes(map_id=tolower(stateName))) +
   geom_map(map=us,aes(fill=df$Murder)) +
   expand_limits(x=us$long,y=us$lat) +
