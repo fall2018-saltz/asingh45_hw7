06679c2ba7f5f0e3b9e174d3144a75115d8f3079
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 20:06:11 -0400

    Saving merge_df.R

--- merge_df.R
+++ merge_df.R
@@ -1,1 +1,5 @@
 
+#Here we merge our two dataframes by using the merge function in R 
+#which uses a common column for merging which in this case is the stateName column
+merged_df <- merge(x=census,y=arrests)
+
