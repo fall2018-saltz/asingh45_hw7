8fe550b1e83adc05876c91e3f59eba4071afe484
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:31:07 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -30,30 +30,26 @@ usa_area <- ggplot(df,aes(map_id=tolower(stateName))) +
   expand_limits(x=us$long,y=us$lat) +
   coord_map() +
   ggtitle('USA Area')
-ggsave('usa_area.png')
 
 #Step C: Create a color shaded map of the U.S. based on 
 #the Murder rate for each state 
 #In this case we use the above approach but fill our map
 #with the murder rate variable
-ggplot(df,aes(map_id=tolower(stateName))) +
+usa_murder_rate <- ggplot(df,aes(map_id=tolower(stateName))) +
   geom_map(map=us,aes(fill=df$Murder)) +
   expand_limits(x=us$long,y=us$lat) +
   coord_map() +
   ggtitle('USA Murder Rate')
-ggsave('usa_murder_rate.png')
 
 #To further add Population using points we use geom_point geometry
 #and set the size argument as population values, also we place
 #these points at the center of states using the center
 #lat-long saved previously
-ggplot(df,aes(map_id=tolower(stateName))) +
+usa_pop <- ggplot(df,aes(map_id=tolower(stateName))) +
   geom_map(map=us,aes(fill=df$StateArea)) +
   geom_point(aes(x=df$StateCenterX,y=df$StateCenterY,size=df$population)) +
   expand_limits(x=us$long,y=us$lat) +
   coord_map() +
-  ggtitle('USA Population')
-ggsave('usa_pop.png')
 
 #Step D: Zoom the map
 #to zoom the map we merely adjust the axis limits to make our visualization
@@ -72,12 +68,10 @@ usa_ne_mr <-  ggplot(df,aes(map_id=tolower(stateName))) +
   expand_limits(x=us$long,y=us$lat) +
   coord_map(xlim=c(nyx-10,nyx+10),ylim=c(nyy-10,nyy+10)) +
   ggtitle('North East Murder Rate')
-ggsave('usa_ne_mr.png')
 
-ggplot(df,aes(map_id=tolower(stateName))) +
+usa_ne_pop <- ggplot(df,aes(map_id=tolower(stateName))) +
   geom_map(map=us,aes(fill=df$StateArea)) +
   geom_point(aes(x=df$StateCenterX,y=df$StateCenterY,size=df$population)) +
   expand_limits(x=us$long,y=us$lat) +
   coord_map(xlim=c(nyx-10,nyx+10),ylim=c(nyy-10,nyy+10)) +
   ggtitle('North East Population')
-ggsave('usa_ne_pop.png')
